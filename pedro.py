839fed02a997bc5ee708e155e372ee21edc3dc85
pedrodev <pedro751ok@gmail.com>
2026-03-09 12:39:32 -0300

    Delete pedro.py

--- pedro.py
+++
@@ -1,9 +0,0 @@
-def validar_cpf(cpf):
-    cpf = int(input('digite seu cpf: '))
-    cpf = ''.join(filter(str.isdigit))
-    if len(cpf) != 11:
-        print('seu cpf nao esta completo')
-    if cpf == cpf * len(cpf):
-        print('cpf invalido')
-
-validar_cpf()
